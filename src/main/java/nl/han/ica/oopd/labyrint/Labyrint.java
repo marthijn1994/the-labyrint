138587fb8b609847fec39f203d6759dfbdd7cb4a
Marthijn <marthijn1994@gmail.com>
2019-03-22 09:49:54 +0100

    Added Main Program

---
+++ src/main/java/nl/han/ica/oopd/labyrint/Labyrint.java
@@ -0,0 +1,30 @@
+package nl.han.ica.oopd.labyrint;
+
+import nl.han.ica.oopg.engine.GameEngine;
+import processing.core.PApplet;
+
+@SuppressWarnings("serial")
+public class Labyrint extends GameEngine {
+
+	public static void main(String[] args) {
+		String[] processingArgs = {"nl.han.ica.oopd.waterworld.WaterWorld"};
+		Labyrint mySketch = new Labyrint();
+
+        PApplet.runSketch(processingArgs, mySketch);
+	}
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
