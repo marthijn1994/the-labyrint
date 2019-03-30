da1a2afa9b167b2c4c1e0d96c82c0e27cb03aefd
Marthijn Kip <marthijn1994@gmail.com>
2019-03-30 21:35:20 +0100

    small corrections

--- src/main/java/nl/han/ica/oopd/labyrint/utils/Direction.java
+++ src/main/java/nl/han/ica/oopd/labyrint/utils/Direction.java
@@ -6,12 +6,12 @@ import nl.han.ica.oopg.exceptions.TileNotFoundException;
 import nl.han.ica.oopg.tile.Tile;
 
 public class Direction {
-	
+
 	public static final int NORTH = 0;
 	public static final int EAST = 90;
 	public static final int SOUTH = 180;
 	public static final int WEST = 270;
-	
+
 	/**
 	 * Bereken de correct direction voor de enemies
 	 * 
@@ -21,30 +21,29 @@ public class Direction {
 	 * @return direction
 	 */
 	public static float calculateDirection(Labyrint world, int tileY, int tileX) {
-		int direction = Direction.SOUTH;
-		
+		float direction = SOUTH;
+
 		try {
 			Tile topTile = world.getTileMap().getTileOnIndex(tileX, tileY - 1);
 			Tile rightTile = world.getTileMap().getTileOnIndex(tileX + 1, tileY);
 			Tile bottomTile = world.getTileMap().getTileOnIndex(tileX, tileY + 1);
 			Tile leftTile = world.getTileMap().getTileOnIndex(tileX - 1, tileY);
-			
+
 			boolean topIsSolid = topTile instanceof SolideTile;
 			boolean leftIsSolid = leftTile instanceof SolideTile;
 			boolean bottomIsSolid = bottomTile instanceof SolideTile;
 			boolean rightIsSolid = rightTile instanceof SolideTile;
-			
+
 			if (!topIsSolid) {
-				direction = Direction.NORTH;
+				direction = NORTH;
 			} else if (!rightIsSolid) {
-				direction = Direction.EAST;
+				direction = EAST;
 			} else if (!bottomIsSolid) {
-				direction = Direction.SOUTH;
+				direction = SOUTH;
+			} else if (!leftIsSolid) {
+				direction = WEST;
 			}
-			if (!leftIsSolid) {
-				direction = Direction.WEST;
-			}
-			
+
 		} catch (TileNotFoundException e) {
 			e.printStackTrace();
 		}
