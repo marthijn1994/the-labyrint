fb4bfa07a3c1b67fca84b055b5cced3e5ce3b904
Werner van Voorst <WDJ.vanVoorst@student.han.nl>
2019-04-02 14:53:45 +0200

    Added some comments
    Kind of pointless, but also pointless to delete, so...

--- src/main/java/nl/han/ica/oopd/labyrint/Inventory.java
+++ src/main/java/nl/han/ica/oopd/labyrint/Inventory.java
@@ -10,38 +10,55 @@ import java.util.List;
 
 import nl.han.ica.oopd.labyrint.items.Diamond;
 import nl.han.ica.oopd.labyrint.items.Key;
-
+/**
+ * 
+ * @author Wern212
+ *
+ */
 public class Inventory {
 
 	private List<Diamond> diamonds;
 	private List<Key> keys;
-
+	
 	public Inventory() {
 		diamonds = new ArrayList<Diamond>();
 		keys = new ArrayList<Key>();
 	}
-
+/**
+ * 
+ * @return
+ */
 	public List<Diamond> getDiamonds() {
 		return this.diamonds;
 	}
-
+/**
+ * 
+ * @return
+ */
 	public List<Key> getKeys() {
 		return this.keys;
 	}
-
+/**
+ * 
+ * @param diamond
+ */
 	public void addDiamond(Diamond diamond) {
 		diamonds.add(diamond);
 	}
-
+/**
+ * 
+ * @param key
+ */
 	public void addKey(Key key) {
 		keys.add(key);
 	}
-	
+/**
+ * 	
+ */
 	public void clearInventory() {
 		diamonds.clear();
 		keys.clear();
 	}
-
 	@Override
 	public String toString() {
 		return String.format("Aantal keys: %s, aantal diamands: %s", keys.size(), diamonds.size());
